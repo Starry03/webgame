25989d61fa3d1c36ac37675d8bb0d24f05592924
ivanVic03 <viceconti.2043692@studenti.uniroma1.it>
2025-04-07 17:31:22 +0200

    new handling character system

---
+++ webgame/src/pages/game/scelta_personaggio/handleChoosingCharacters.vue
@@ -0,0 +1,16 @@
+<template>
+    <section class="container">
+        <h1 id="container-title">Choose your character</h1>
+        <div id="characters-box" v-for="character in characters" :key="character.id" @click="selectCharacter"(character) >
+
+        </div>
+    </section>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+	
+</style>
