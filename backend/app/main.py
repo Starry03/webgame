4fb3b7e91a764acaef53237c17ac863a85aa3bb5
ivanVic03 <viceconti.2043692@studenti.uniroma1.it>
2025-04-18 16:02:29 +0200

    changes on main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,29 +1,3 @@
-<<<<<<< HEAD
-from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
-
-from .routes.auth.login import router as auth_router
-from .routes.game.router import router as game_router
-from .security.middleware.rsa_middleware import RSADecryptionMiddleware
-from .security.middleware.aes_middleware import AESDecryptionMiddleware
-
-app = FastAPI()
-
-# middleware
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["localhost", "http://localhost:3000"],
-    allow_methods=["*"],
-    allow_headers=["*"],
-    allow_credentials=True,
-)
-app.add_middleware(RSADecryptionMiddleware)
-app.add_middleware(AESDecryptionMiddleware)
-
-# router
-app.include_router(auth_router)
-app.include_router(game_router)
-=======
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
@@ -48,4 +22,3 @@ app.add_middleware(AESDecryptionMiddleware)
 # router
 app.include_router(auth_router)
 app.include_router(game_router)
->>>>>>> 73cfda6170f43e068e747257e5d0c58cf6417bba
