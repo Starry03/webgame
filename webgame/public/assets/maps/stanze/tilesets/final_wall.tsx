a267ca24ac54eac78c2dd44cac91bf5bc17f68a9
ivanVic03 <viceconti.2043692@studenti.uniroma1.it>
2025-05-02 19:10:45 +0200

    maps for the game

---
+++ webgame/public/assets/maps/stanze/tilesets/final_wall.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="final_wall" tilewidth="32" tileheight="32" spacing="1" tilecount="1" columns="1">
+ <image source="../oggetti/final_wall.png" width="38" height="38"/>
+</tileset>
---
+++ webgame/public/assets/maps/stanze/tilesets/rocks.tsx
@@ -0,0 +1,16 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="rocks" tilewidth="58" tileheight="29" tilecount="4" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image source="../oggetti/rocce/rock1.png" width="58" height="29"/>
+ </tile>
+ <tile id="1">
+  <image source="../oggetti/rocce/rock2.png" width="23" height="14"/>
+ </tile>
+ <tile id="2">
+  <image source="../oggetti/rocce/rock3.png" width="26" height="20"/>
+ </tile>
+ <tile id="3">
+  <image source="../oggetti/rocce/rock4.png" width="32" height="28"/>
+ </tile>
+</tileset>
