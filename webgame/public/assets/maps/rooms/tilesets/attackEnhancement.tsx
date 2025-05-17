075d9be4685989a54c627cee7f8b88a1dbd1c8e0
ivanVic03 <viceconti.2043692@studenti.uniroma1.it>
2025-05-17 22:38:57 +0200

    updates about rooms' maps

--- webgame/public/assets/maps/rooms/tilesets/attackEnhancement.tsx
+++ webgame/public/assets/maps/rooms/tilesets/attackEnhancement.tsx
@@ -2,6 +2,6 @@
 <tileset version="1.10" tiledversion="1.11.2" name="attackEnhancement" tilewidth="536" tileheight="465" tilecount="1" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="2">
-  <image source="../tiled_objects/attack_enchantment.png" width="536" height="465"/>
+  <image source="../tiled_objects/attack_enhancement.png" width="536" height="465"/>
  </tile>
 </tileset>
