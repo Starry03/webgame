b079fba575caa4b68b4b30ff142c26d04abc7d7f
ivanVic03 <viceconti.2043692@studenti.uniroma1.it>
2025-05-21 14:09:31 +0200

    updates about stats

--- webgame/src/components/StatusBar.vue
+++ webgame/src/components/StatusBar.vue
@@ -60,6 +60,19 @@ const manaPercentage = computed(() => {
 
 const storedUser = localStorage.getItem(Storage_e.USER)
 const username = storedUser ? JSON.parse(storedUser)?.username ?? 'Player' : 'Player'
+
+const getHealthPercentage = () => {
+    return healthPercentage.value
+}
+
+const getManaPercentage = () => {
+    return manaPercentage.value
+}
+
+const getLevel = () => {
+    return props.level;
+}
+
 </script>
 
 <template>
