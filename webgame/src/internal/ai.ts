c86fb3005fdbf634cc4550b69140f9b8ef166a45
Starry03 <andrea.starrantino@gmail.com>
2025-05-26 01:13:27 +0200

    refactor: streamline enemy movement logic in Ai update method

--- webgame/src/internal/ai.ts
+++ webgame/src/internal/ai.ts
@@ -32,55 +32,61 @@ export class Ai {
     }
 
     update(deltaTime: number) {
-        let wantedPosition: WeightedMove[] = []
-        if (this.player.isDead) return
-        this.enemies.forEach((enemy: Entity) => {
-            if (enemy.isDead) return
-            if (
-                !enemy
-                    .getRelDirection(this.player)
-                    .direction()
-                    .compare(enemy.facingDirection.x, enemy.facingDirection.y)
-            )
-                enemy.turn(enemy.getRelDirection(this.player))
-            if (
-                Array.from(enemy.collidedObjects).some(
-                    (o: CollisionInfo) => o.other.id === this.player.id,
-                )
-            ) {
-                enemy.attack(this.keys)
+        return new Promise<void>((resolve) => {
+            let wantedPosition: WeightedMove[] = []
+            if (this.player.isDead) {
+                resolve()
+                return
             }
-            const moves: WeightedMove[] = []
-            this.moveCombinations.forEach((comb) => {
-                const moveResult = enemy.move(new Set(comb.split('')), deltaTime, true)
-                if (moveResult && moveResult.possible_position && moveResult.dir) {
-                    const wantedPos = moveResult.possible_position
-                    const dir = moveResult.dir
-                    const weight = this.heuristic(wantedPos, dir, enemy, this.player)
-                    moves.push({
-                        object: enemy,
-                        position: wantedPos,
-                        weight: weight,
-                        move: comb,
-                    } as WeightedMove)
-                }
-            })
-            moves.sort((a, b) => a.weight - b.weight)
-            if (moves.length === 0) return
-            moves.forEach((move: WeightedMove) => {
-                const hasCollisions = wantedPosition.map((wp: WeightedMove) => {
-                    return Collider.collides(
-                        move.position,
-                        move.object.pos,
-                        wp.position,
-                        wp.object.pos,
+            this.enemies.forEach((enemy: Entity) => {
+                if (enemy.isDead) return
+                if (
+                    !enemy
+                        .getRelDirection(this.player)
+                        .direction()
+                        .compare(enemy.facingDirection.x, enemy.facingDirection.y)
+                )
+                    enemy.turn(enemy.getRelDirection(this.player))
+                if (
+                    Array.from(enemy.collidedObjects).some(
+                        (o: CollisionInfo) => o.other.id === this.player.id,
                     )
+                ) {
+                    enemy.attack(this.keys)
+                }
+                const moves: WeightedMove[] = []
+                this.moveCombinations.forEach((comb) => {
+                    const moveResult = enemy.move(new Set(comb.split('')), deltaTime, true)
+                    if (moveResult && moveResult.possible_position && moveResult.dir) {
+                        const wantedPos = moveResult.possible_position
+                        const dir = moveResult.dir
+                        const weight = this.heuristic(wantedPos, dir, enemy, this.player)
+                        moves.push({
+                            object: enemy,
+                            position: wantedPos,
+                            weight: weight,
+                            move: comb,
+                        } as WeightedMove)
+                    }
                 })
-                if (!hasCollisions.includes(true)) wantedPosition.push(move)
+                moves.sort((a, b) => a.weight - b.weight)
+                if (moves.length === 0) return
+                moves.forEach((move: WeightedMove) => {
+                    const hasCollisions = wantedPosition.map((wp: WeightedMove) => {
+                        return Collider.collides(
+                            move.position,
+                            move.object.pos,
+                            wp.position,
+                            wp.object.pos,
+                        )
+                    })
+                    if (!hasCollisions.includes(true)) wantedPosition.push(move)
+                })
             })
-        })
-        wantedPosition.forEach((move: WeightedMove) => {
-            move.object.move(new Set(move.move.split('')), deltaTime)
+            wantedPosition.forEach((move: WeightedMove) => {
+                move.object.move(new Set(move.move.split('')), deltaTime)
+            })
+            resolve()
         })
     }
 }
